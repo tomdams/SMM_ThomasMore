9353566cd5b1d45e86eb510fb434ed8e03f43caf
MaartenVissers <maarten.vissers@student.kdg.be>
2018-03-30 15:18:56 +0200

    reactie op de knop: signin

--- AndroidSMM/app/src/main/java/com/example/android_smm/Activity_Login.java
+++ AndroidSMM/app/src/main/java/com/example/android_smm/Activity_Login.java
@@ -1,14 +1,22 @@
 package com.example.android_smm;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Activity_Login extends AppCompatActivity {
-    @BindView(R.id.button_signin) Button btn_signin;
+    @BindView(R.id.button_signin)
+    Button buttonSignin;
+    @BindView(R.id.editText_username)
+    EditText editTextUsername;
+    @BindView(R.id.editText_password)
+    EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +26,12 @@ public class Activity_Login extends AppCompatActivity {
 
     }
 
-    private void initializeComponents(){
-        //button_signin = view
+    @OnClick(R.id.button_signin)
+    public void onViewClicked() {
+        if(!editTextPassword.getText().toString().isEmpty() && !editTextUsername.getText().toString().isEmpty()){
+            Intent intent = new Intent(Activity_Login.this, Dashboard_Activity.class);
+            startActivity(intent);
+        }
+
     }
 }
