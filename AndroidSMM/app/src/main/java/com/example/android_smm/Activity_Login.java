2e2fd583a9513af47a6fd9231571684e82443d24
MaartenVissers <maarten.vissers@student.kdg.be>
2018-03-30 14:59:09 +0200

    ButterKnife Implementation

--- AndroidSMM/app/src/main/java/com/example/android_smm/Activity_Login.java
+++ AndroidSMM/app/src/main/java/com/example/android_smm/Activity_Login.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class Activity_Login extends AppCompatActivity {
-    private Button button_signin;
+    @BindView(R.id.button_signin) Button btn_signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__login);
+        ButterKnife.bind(this);
+
     }
 
     private void initializeComponents(){
