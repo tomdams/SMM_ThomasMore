47a1d5a3881200d7857ee14513ae32ed25f68e44
Tom Dams <tom.dams@student.kdg.be>
2018-04-20 15:36:11 +0200

    feelsfacebookman

--- SMM_ThomasMore/SMM_ThomasMore/Controllers/UserController.cs
+++ SMM_ThomasMore/SMM_ThomasMore/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using SMM_ThomasMore.BL;
+﻿using Facebook;
+using SMM_ThomasMore.BL;
 using SMM_ThomasMore.Domain;
 using SMM_ThomasMore.Models;
 using SMM_ThomasMore.Session;
@@ -21,6 +22,7 @@ namespace SMM_ThomasMore.Controllers
     public UserController()
     {
       umgr = new UserManager();
+
     }
 
     // GET: User
@@ -32,14 +34,23 @@ namespace SMM_ThomasMore.Controllers
     [HttpPost]
     public ActionResult AanmeldenPage(LoginVM loginVM)
     {
-     
+      
       if (ModelState.IsValid)
       {
         User authenticatedUser = umgr.getUser(loginVM.username, loginVM.wachtwoord);
         if (authenticatedUser != null)
         {
           currentUser = authenticatedUser;
-          context.SetAuthenticationToken(authenticatedUser.id.ToString(), false, authenticatedUser);
+          // context.SetAuthenticationToken(authenticatedUser.id.ToString(), false, authenticatedUser);
+          var authTicket = new FormsAuthenticationTicket(1, authenticatedUser.username, DateTime.Now, DateTime.Now.AddMinutes(30), true, authenticatedUser.type.ToString().ToLower());
+          string cookieContents = FormsAuthentication.Encrypt(authTicket);
+          var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, cookieContents)
+          {
+            Expires = authTicket.Expiration,
+            Path = FormsAuthentication.FormsCookiePath
+          };
+          Response.Cookies.Add(cookie);
+
           return RedirectToAction("Index", "Home");
         }
       }
@@ -83,5 +94,66 @@ namespace SMM_ThomasMore.Controllers
       FormsAuthentication.SignOut();
       return RedirectToAction("Index", "Home");
     }
+
+
+    private Uri RedirectUri
+    {
+      get
+      {
+        var uriBuilder = new UriBuilder(Request.Url);
+        uriBuilder.Query = null;
+        uriBuilder.Fragment = null;
+        uriBuilder.Path = Url.Action("FacebookCallback");
+        return uriBuilder.Uri;
+      }
     }
+
+    [AllowAnonymous]
+    public ActionResult Facebook()
+    {
+      var fb = new FacebookClient();
+      var loginUrl = fb.GetLoginUrl(new
+      {
+        client_id = "378068569269286",
+        client_secret = "c1d9470875260a1eb344e849022d9607",
+        redirect_uri = RedirectUri.AbsoluteUri,
+        response_type = "code",
+        scope = "email"
+      });
+
+      return Redirect(loginUrl.AbsoluteUri);
+    }
+
+    public ActionResult FacebookCallback(string code)
+    {
+      var fb = new FacebookClient();
+      dynamic result = fb.Post("oauth/access_token", new
+      {
+        client_id = "378068569269286",
+        client_secret = "c1d9470875260a1eb344e849022d9607",
+        redirect_uri = RedirectUri.AbsoluteUri,
+        code = code
+      });
+
+      var accessToken = result.access_token;
+
+      // Store the access token in the session for farther use
+      Session["AccessToken"] = accessToken;
+
+      // update the facebook client with the access token so
+      // we can make requests on behalf of the user
+      fb.AccessToken = accessToken;
+
+      // Get the user's information, like email, first name, middle name etc
+      dynamic me = fb.Get("me?fields=first_name,middle_name,last_name,id,email");
+      string email = me.email;
+      string firstname = me.first_name;
+      string middlename = me.middle_name;
+      string lastname = me.last_name;
+
+      // Set the auth cookie
+      FormsAuthentication.SetAuthCookie(email, false);
+      return RedirectToAction("Index", "Home");
+    }
+  }
 }
