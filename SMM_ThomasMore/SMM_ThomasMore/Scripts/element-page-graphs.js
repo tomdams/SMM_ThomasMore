a431105c526bbfaf89693a2718f098792909d0b8
Tom Dams <tom.dams@student.kdg.be>
2018-05-08 09:28:21 +0200

    persoonpage grafiek

---
+++ SMM_ThomasMore/SMM_ThomasMore/Scripts/element-page-graphs.js
@@ -0,0 +1,40 @@
+﻿ google.charts.load('current', {'packages':['corechart']});
+ google.charts.setOnLoadCallback(drawChart);
+
+function drawChart(data,options) {
+    $.ajax(
+        {
+            type: 'POST',
+            data: JSON.stringify(data),
+            url: '/UIElement/getPolariteit',
+            success:
+            function (response) {
+                var options =
+                    {
+                        width: 1100,
+                        height: 900,
+                        sliceVisibilityThreshold: 0,
+                        legend: { position: "top", alignment: "end" },
+                        chartArea: { left: 370, top: 50, height: "90%" },
+                        bar: { groupWidth: "50%" },
+                        colors: ['#00FF00','#FF0000']
+                    };
+
+                var data = google.visualization.arrayToDataTable([
+                    ['test', "test"],
+                    ['Positief', 50 + ((response / 2)*100)],
+                    ['Negatief', 50 - ((response / 2)*100)]
+                ]);
+
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+                chart.draw(data, options);
+
+            },
+            error: function (ex) {
+                console.log(ex);
+                console.log(ex.responseText);
+                console.log('memes');
+            }
+        });
+   
+}
